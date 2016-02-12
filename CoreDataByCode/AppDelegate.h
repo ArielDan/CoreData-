d5544ff8f3d4b62da6282f9b781ceb2fafa04fff
ariel <xdanealine@gmail.com>
2016-02-13 00:30:20 +0800

    Initial Commit

---
+++ CoreDataByCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataByCode
+//
+//  Created by csip on 16/2/13.
+//  Copyright © 2016年 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataByCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataByCode
+//
+//  Created by csip on 16/2/13.
+//  Copyright © 2016年 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
